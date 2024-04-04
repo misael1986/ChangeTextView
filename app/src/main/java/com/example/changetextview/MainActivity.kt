b0a429c41f17676fd9f2939658d7a3fcff4d3442
misael.perilla <misael.perilla@lemonade.com>
2024-04-03 21:01:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/changetextview/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.changetextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        var contador=0;
+
+        button.setOnClickListener{
+            contador++;
+            textView.text=contador.toString()
+        }
+
+    }
+}
